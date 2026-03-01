feb4b21ca92ea7040344283263a4244fe6f75070
phengsopanhahero-create <phengsopanha.hero@gmail.com>
2026-03-01 17:46:53 +0700

    Remove footer

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -5,7 +5,6 @@ import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -17,7 +16,6 @@ export default function Home() {
       <Projects />
       <Skills />
       <Contact />
-      <Footer />
     </main>
   );
 }
